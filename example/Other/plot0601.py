9f7bb677dbd700d9594062bcd8260da9afef41ab
YotaFujii1 <fujii@hapis.k.u-tokyo.ac.jp>
2025-06-06 12:11:32 +0900

    関数追加

--- example/Other/plot0601.py
+++ example/Other/plot0601.py
@@ -4,21 +4,21 @@ import matplotlib.pyplot as plt
 fs = 2000
 x = np.linspace(0, 1, fs, endpoint=False)
 
-for f1 in {2, 5, 10}:
-    for f2 in {10, 40, 200}:
-        # if f1 != f2:
+for f1 in {6}:
+    for f2 in range(10, 210, 10):
+        if f1 != f2:
             envelope = np.sin(2 * np.pi * f1 * x)
             carrier = np.sin(2 * np.pi * f2 * x)
             y = envelope * carrier
 
             plt.figure(figsize=(10, 4))
-            plt.plot(x, y, label='modulated wave')
-            plt.plot(x, envelope, 'r--', label='envelope')
+            plt.plot(x, y ** 2, label='modulated wave^2')
+            plt.plot(x, envelope ** 2, 'r--', label='envelope^2')
             plt.xlabel('time(s)')
             plt.ylabel('Amplitude')
-            plt.title('modulated wave')
+            plt.title(f'radiation pressure (eFreq:{f1} cFreq:{f2})')
             plt.legend()
             plt.xlim(0, 1)
             plt.tight_layout()
-            plt.savefig(f"example/Other/img0601/modulated_wave_e{f1}Hz_c{f2}Hz.png")
+            plt.savefig(f"example/Other/img0601/rp_e{f1}Hz_c{f2}Hz.png")
             plt.close()
---
+++ example/Runner_sai/Runner0606_constEnvelope.cs
@@ -0,0 +1,109 @@
+// Runner0522.cs
+using System;
+using System.Diagnostics;
+using System.Threading;
+using AUTD3Sharp;
+using AUTD3Sharp.Gain;
+using AUTD3Sharp.Modulation;
+using AUTD3Sharp.Utils;
+using static AUTD3Sharp.Units;
+
+
+namespace Runner_sai
+{
+    public static class Runner0606_constEnvelope
+    {
+        // 整数の最大公約数をユークリッドの互除法で計算
+        static int Gcd(int a, int b)
+        {
+            return b == 0 ? a : Gcd(b, a % b);
+        }
+        public static void Run(Controller autd)
+        {
+            // 無音状態の初期化
+            autd.Send(new Silencer());
+
+            // 焦点位置設定
+            var focus = new Focus(
+                pos: autd.Center() + new Vector3(0, 0, 150),
+                option: new FocusOption()
+            );
+
+            // 焦点位置確認用
+            var m = new Sine(freq: 150 * Hz, option: new SineOption());
+            autd.Send((m, focus));
+            Thread.Sleep(15000);
+            autd.Send((new Silencer(), new Null()));
+            Thread.Sleep(3000);
+
+            // int[] carrierList = {  };
+            int[] envelopeList = { 6 };
+
+            
+            foreach (int eFreq in envelopeList)
+            {
+                Console.WriteLine($"→envelope={eFreq}Hz");
+
+                var m = new Sine(freq: 150 * Hz, option: new SineOption());
+                autd.Send((m, focus));
+                Thread.Sleep(10000);
+                autd.Send((new Silencer(), new Null()));
+
+                for (int cFreq = 10; cFreq < 201; cFreq += 10)
+                {
+                    Console.WriteLine($"→ carrier={cFreq}Hz, envelope={eFreq}Hz");
+
+                    // ① 波形生成
+                    const int sampleRate = 1000; // サンプリング周波数 
+                    int gcd = Gcd(cFreq, eFreq);  // carrier と envelope の最大公約数
+                    int periodSamples = sampleRate / gcd; // 波形周期: 1/gcd(s) * sampleRate(data/s)
+
+                    var buffer = new byte[periodSamples];
+                    for (int i = 0; i < periodSamples; i++)
+                    {
+                        double t = i / (double)sampleRate;
+
+                        double carrier = Math.Sin(2 * Math.PI * cFreq * t);
+                        double envelope = Math.Sin(2 * Math.PI * eFreq * t);
+                        double val = carrier * envelope;
+
+                        int intLevel = (int)Math.Round((val * 0.5 + 0.5) * 255.0);
+                        buffer[i] = (byte)intLevel;
+                    }
+
+                    // ② 波形送信(10s)
+                    var wave = new AUTD3Sharp.Modulation.Custom(
+                        buffer: buffer,
+                        samplingConfig: 1000f * Hz
+                    );
+                    autd.Send((wave, focus));
+
+                    Thread.Sleep(10000);
+                    autd.Send((new Silencer(), new Null()));
+                    Console.WriteLine("一個前の波と同じ:s 異なる:d / 終了:enter");
+
+                    // ③ 有効なキーが押されるまで繰り返し待ち
+                    while (true)
+                    {
+                        var key = Console.ReadKey(true).Key;
+                        if (key == ConsoleKey.Enter)
+                        {
+                            Console.WriteLine("終了します");
+                            autd.Close();
+                            return;
+                        }
+                        if (key == ConsoleKey.D || key == ConsoleKey.S)
+                        {
+                            Console.WriteLine($"{key}");
+                            break;
+                        }
+                        Console.WriteLine("無効なキーです。Enter または d/s を押してください");
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+

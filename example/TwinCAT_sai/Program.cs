555b34e71bddfa4edf3530c4d6078f4a8b08e77f
YotaFujii1 <fujii@hapis.k.u-tokyo.ac.jp>
2025-07-13 22:40:51 +0900

    謎エラー

--- example/TwinCAT_sai/Program.cs
+++ example/TwinCAT_sai/Program.cs
@@ -9,73 +9,73 @@ using Runner_sai;
 
 namespace TwinCAT_sai
 {
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            // System.Environment.SetEnvironmentVariable("RUST_LOG", "autd3=DEBUG");
-            // AUTD3Sharp.Tracing.Init();
+                static void Main(string[] args)
+                {
+                        // System.Environment.SetEnvironmentVariable("RUST_LOG", "autd3=DEBUG");
+                        // AUTD3Sharp.Tracing.Init();
 
-            private const float OscillatorRadius = 0.00508f; // 振動子の半径 AUTD位置設定用
-            private const float width_x_right = 0.0004f;  // 向かって右側のAUTDと正面のAUTDのAUTD座標におけるx軸方向のずれ(正面のAUTDのtransducerの端に対する、右側のAUTDのtransducerの面の位置)
-            private const float width_z_right = 0.0005f;  // 向かって右側のAUTDと正面のAUTDのAUTD座標におけるy軸方向のずれ(正面のAUTDのtransducerの面に対する、右側のAUTDのtransducerの端の位置)
-            private const float width_x_left = 0.001f;  // 向かって左側のAUTDと正面のAUTDのAUTD座標におけるx軸方向のずれ(正面のAUTDのtransducerの端に対する、左側のAUTDのtransducerの面の位置)
-            private const float width_z_left = 0.0005f;  // 向かって左側のAUTDと正面のAUTDのAUTD座標におけるy軸方向のずれ(正面のAUTDのtransducerの面に対する、左側のAUTDのtransducerの端の位置)
-            private const float WidthOriginToOscillatorEdge = 0.1778f; //AUTDの原点位置から、width方向(x軸)に進んで、一番端のオシレーターの端までの長さ[m]
-
-
-            AUTD3[] devices = new AUTD3[]
-            {
-                // 1~4
-                new AUTD3(
-                        pos: new Point3(-AUTD3.DeviceWidth - OscillatorRadius - width_x_left, 0, width_z_left + WidthOriginToOscillatorEdge + AUTD3.DeviceWidth),
-                        rot: EulerAngles.Zyz(0 * rad, MathF.PI / 2 * rad, 0 * rad)),
-                new AUTD3(
-                        pos: new Point3(-AUTD3.DeviceWidth - OscillatorRadius - width_x_left, -AUTD3.DeviceHeight, width_z_left + WidthOriginToOscillatorEdge + AUTD3.DeviceWidth),
-                        rot: EulerAngles.Zyz(0 * rad, MathF.PI / 2 * rad, 0 * rad)),
-                new AUTD3(
-                        pos: new Point3(-AUTD3.DeviceWidth - OscillatorRadius - width_x_left, -AUTD3.DeviceHeight, width_z_left + WidthOriginToOscillatorEdge),
-                        rot: EulerAngles.Zyz(0 * rad, MathF.PI / 2 * rad, 0 * rad)),
-                new AUTD3(
-                        pos: new Point3(-AUTD3.DeviceWidth - OscillatorRadius - width_x_left, 0, width_z_left + WidthOriginToOscillatorEdge),
-                        rot: EulerAngles.Zyz(0 * rad, MathF.PI / 2 * rad, 0 * rad)),
+                        private const float OscillatorRadius = 0.00508f; // 振動子の半径 AUTD位置設定用
+                        private const float width_x_right = 0.0004f;  // 向かって右側のAUTDと正面のAUTDのAUTD座標におけるx軸方向のずれ(正面のAUTDのtransducerの端に対する、右側のAUTDのtransducerの面の位置)
+                        private const float width_z_right = 0.0005f;  // 向かって右側のAUTDと正面のAUTDのAUTD座標におけるy軸方向のずれ(正面のAUTDのtransducerの面に対する、右側のAUTDのtransducerの端の位置)
+                        private const float width_x_left = 0.001f;  // 向かって左側のAUTDと正面のAUTDのAUTD座標におけるx軸方向のずれ(正面のAUTDのtransducerの端に対する、左側のAUTDのtransducerの面の位置)
+                        private const float width_z_left = 0.0005f;  // 向かって左側のAUTDと正面のAUTDのAUTD座標におけるy軸方向のずれ(正面のAUTDのtransducerの面に対する、左側のAUTDのtransducerの端の位置)
+                        private const float WidthOriginToOscillatorEdge = 0.1778f; //AUTDの原点位置から、width方向(x軸)に進んで、一番端のオシレーターの端までの長さ[m]
 
 
-                // 5~8
-                new AUTD3(
-                        pos: new Point3(-AUTD3.DeviceWidth, 0, 0),
-                        rot: Quaternion.Identity),
-                new AUTD3(
-                        pos: new Point3(-AUTD3.DeviceWidth, -AUTD3.DeviceHeight, 0),
-                        rot: Quaternion.Identity),
-                new AUTD3(
-                        pos: new Point3(0, -AUTD3.DeviceHeight, 0),
-                        rot: Quaternion.Identity),
-                new AUTD3(
-                        pos: Point3.Origin,
-                        rot: Quaternion.Identity),
+                        AUTD3[] devices = new AUTD3[]
+                        {
+                                // 1~4
+                                new AUTD3(
+                                        pos: new Point3(-AUTD3.DeviceWidth - OscillatorRadius - width_x_left, 0, width_z_left + WidthOriginToOscillatorEdge + AUTD3.DeviceWidth),
+                                        rot: EulerAngles.Zyz(0 * rad, MathF.PI / 2 * rad, 0 * rad)),
+                                new AUTD3(
+                                        pos: new Point3(-AUTD3.DeviceWidth - OscillatorRadius - width_x_left, -AUTD3.DeviceHeight, width_z_left + WidthOriginToOscillatorEdge + AUTD3.DeviceWidth),
+                                        rot: EulerAngles.Zyz(0 * rad, MathF.PI / 2 * rad, 0 * rad)),
+                                new AUTD3(
+                                        pos: new Point3(-AUTD3.DeviceWidth - OscillatorRadius - width_x_left, -AUTD3.DeviceHeight, width_z_left + WidthOriginToOscillatorEdge),
+                                        rot: EulerAngles.Zyz(0 * rad, MathF.PI / 2 * rad, 0 * rad)),
+                                new AUTD3(
+                                        pos: new Point3(-AUTD3.DeviceWidth - OscillatorRadius - width_x_left, 0, width_z_left + WidthOriginToOscillatorEdge),
+                                        rot: EulerAngles.Zyz(0 * rad, MathF.PI / 2 * rad, 0 * rad)),
 
-                // 9~12
-                new AUTD3(
-                        pos: new Point3(WidthOriginToOscillatorEdge + width_x_right, 0, width_z_right + OscillatorRadius),
-                        rot: EulerAngles.Zyz(0 * rad, - MathF.PI / 2 * rad, 0 * rad)),
-                new AUTD3(
-                        pos: new Point3(WidthOriginToOscillatorEdge + width_x_right, -AUTD3.DeviceHeight, width_z_right + OscillatorRadius),
-                        rot: EulerAngles.Zyz(0 * rad, - MathF.PI / 2 * rad, 0 * rad)),
-                new AUTD3(
-                        pos: new Point3(WidthOriginToOscillatorEdge + width_x_right, -AUTD3.DeviceHeight, width_z_right + OscillatorRadius + AUTD3.DeviceWidth),
-                        rot: EulerAngles.Zyz(0 * rad, - MathF.PI / 2 * rad, 0 * rad)),
-                new AUTD3(
-                        pos: new Point3(WidthOriginToOscillatorEdge + width_x_right, 0, width_z_right + OscillatorRadius + AUTD3.DeviceWidth),
-                        rot: EulerAngles.Zyz(0 * rad, - MathF.PI / 2 * rad, 0 * rad)),
-            };
 
-            // TwinCAT 経由でコントローラをオープン
-            using var autd = Controller.Open(devices, new TwinCAT());
+                                // 5~8
+                                new AUTD3(
+                                        pos: new Point3(-AUTD3.DeviceWidth, 0, 0),
+                                        rot: Quaternion.Identity),
+                                new AUTD3(
+                                        pos: new Point3(-AUTD3.DeviceWidth, -AUTD3.DeviceHeight, 0),
+                                        rot: Quaternion.Identity),
+                                new AUTD3(
+                                        pos: new Point3(0, -AUTD3.DeviceHeight, 0),
+                                        rot: Quaternion.Identity),
+                                new AUTD3(
+                                        pos: Point3.Origin,
+                                        rot: Quaternion.Identity),
 
-            // Runner で一連のデモを実行
-            Runner0622_soundpressure.Run(autd);
-            
+                                // 9~12
+                                new AUTD3(
+                                        pos: new Point3(WidthOriginToOscillatorEdge + width_x_right, 0, width_z_right + OscillatorRadius),
+                                        rot: EulerAngles.Zyz(0 * rad, - MathF.PI / 2 * rad, 0 * rad)),
+                                new AUTD3(
+                                        pos: new Point3(WidthOriginToOscillatorEdge + width_x_right, -AUTD3.DeviceHeight, width_z_right + OscillatorRadius),
+                                        rot: EulerAngles.Zyz(0 * rad, - MathF.PI / 2 * rad, 0 * rad)),
+                                new AUTD3(
+                                        pos: new Point3(WidthOriginToOscillatorEdge + width_x_right, -AUTD3.DeviceHeight, width_z_right + OscillatorRadius + AUTD3.DeviceWidth),
+                                        rot: EulerAngles.Zyz(0 * rad, - MathF.PI / 2 * rad, 0 * rad)),
+                                new AUTD3(
+                                        pos: new Point3(WidthOriginToOscillatorEdge + width_x_right, 0, width_z_right + OscillatorRadius + AUTD3.DeviceWidth),
+                                        rot: EulerAngles.Zyz(0 * rad, - MathF.PI / 2 * rad, 0 * rad)),
+                        };
+
+                // TwinCAT 経由でコントローラをオープン
+                using var autd = Controller.Open(devices, new TwinCAT());
+
+                // Runner で一連のデモを実行
+                Runner0622_soundpressure.Run(autd);
+                
         }
     }
 }
